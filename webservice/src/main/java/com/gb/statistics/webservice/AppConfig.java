9470c134a7dc7c446afdd176a4611b9b65de14bb
ZeroCool142 <undead142@gmail.com>
2017-06-07 23:35:40 +0300

    create spring project template

---
+++ webservice/src/main/java/com/gb/statistics/webservice/AppConfig.java
@@ -0,0 +1,11 @@
+package com.gb.statistics.webservice;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan("com.gb.webservice")
+@EnableWebMvc
+public class AppConfig {
+}
--- webservice/src/main/java/com/gb/statistics/webservice/Application.java
+++
@@ -1,8 +0,0 @@
-package com.gb.statistics.webservice;
-
-
-public class Application {
-    public static void main(String[] args) {
-
-    }
-}
