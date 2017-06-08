65b0de5d599fda38274bd1af1469a234002c68bf
GokenTanmay <gkntnm@gmail.com>
2017-06-08 15:41:19 +0400

    Добавлен класс Parser c заглушкой на подсчет рейтинга.

---
+++ src/crawler/Parser.java
@@ -0,0 +1,10 @@
+package crawler;
+
+/**
+ * Created by Serg on 08.06.2017.
+ */
+public class Parser {
+    public float calculateRating(String site, String person){
+        return (float) 0.0;
+    }
+}
