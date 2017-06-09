672e3c5476926681444c25170a02c38585d2eccb
GokenTanmay <gkntnm@gmail.com>
2017-06-09 14:06:45 +0400

    Создан метод-заглушка getPagesWithoutScanDate()

--- src/crawler/DBHelper.java
+++ src/crawler/DBHelper.java
@@ -6,7 +6,13 @@ import java.util.ArrayList;
  * Created by Serg on 09.06.2017.
  */
 public class DBHelper {
+    private ArrayList<String> pagesWithoutScanDate;
+
     public void addPagesToSite(ArrayList<String> urlPages) {
 
+    }
+
+    public ArrayList<String> getPagesWithoutScanDate() {
+        return new ArrayList<>();
     }
 }
