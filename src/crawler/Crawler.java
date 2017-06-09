81bd3980e8451855217a98ad224b133944b925e1
GokenTanmay <gkntnm@gmail.com>
2017-06-09 09:40:26 +0400

    Косметический рефактор кода. Функционал не изменен.

--- src/crawler/Crawler.java
+++ src/crawler/Crawler.java
@@ -7,46 +7,47 @@ import java.util.ArrayList;
  * Created by Serg on 08.06.2017.
  */
 public class Crawler {
-    private Downloader downloader;
-    private ArrayList<String> newSites;
-    private ArrayList<String> urlsForScan;
-    private ArrayList<String> pagesWithoutScanDate;
+    private static ArrayList<String> urlsForScan;
+    private static Downloader downloader;
+    private static ArrayList<String> newSites;
+    private static ArrayList<String> pagesWithoutScanDate;
 
-    public void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         init();
-
-        newSites = getNewSites();
-        addNewSitesToPages(newSites);
-
-        urlsForScan = getPagesWithoutScanDate();
-        for (String url: urlsForScan) {
-            downloader.download(url);
-            //Коллеги тут вопрос, как и кем будем определять тип получаемого контента? (robot.txt, sitemap, старница)
-        }
-
+        crawlsNewSite();
         recrawls();
     }
 
-    private void recrawls() {
+    private static void crawlsNewSite() throws IOException {
+        newSites = getNewSites();
+        addNewSitesToPages(newSites);
+        urlsForScan = getPagesWithoutScanDate();
+        for (String url : urlsForScan) {
+            downloader.download(url);
+            //Коллеги тут вопрос, как и кем будем определять тип получаемого контента? (robot.txt, sitemap, старница)
+        }
+    }
+
+    private static void recrawls() {
         //Функция повторного обхода
     }
 
-    private void init() {
+    private static void init() {
         downloader = new Downloader();
     }
 
-    private void addNewSitesToPages(ArrayList<String> newSites) {
-        for (String site: newSites) {
+    private static void addNewSitesToPages(ArrayList<String> newSites) {
+        for (String site : newSites) {
             //Добавляем ссылку на robots.txt в таблицу Pages для site
         }
     }
 
-    public ArrayList<String> getNewSites() {
+    private static ArrayList<String> getNewSites() {
         //Получить из базы строки из таблицы Sites, которым не соответсвует НИ ОДНОЙ строки в таблице Pages
         return newSites;
     }
 
-    public ArrayList<String> getPagesWithoutScanDate() {
+    private static ArrayList<String> getPagesWithoutScanDate() {
         //Получить из таблицы Pages все ссылки с LastScanDate равным null
         return pagesWithoutScanDate;
     }
