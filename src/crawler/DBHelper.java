9146b7bd3c982c9436d76ae89a2292f27d4ff60a
GokenTanmay <gkntnm@gmail.com>
2017-06-09 14:01:21 +0400

    Добавлен метода-заглушка addPagesToSite

--- src/crawler/DBHelper.java
+++ src/crawler/DBHelper.java
@@ -1,7 +1,12 @@
 package crawler;
 
+import java.util.ArrayList;
+
 /**
  * Created by Serg on 09.06.2017.
  */
 public class DBHelper {
+    public void addPagesToSite(ArrayList<String> urlPages) {
+
+    }
 }
