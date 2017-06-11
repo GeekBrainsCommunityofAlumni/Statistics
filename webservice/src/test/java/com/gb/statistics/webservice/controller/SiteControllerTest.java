8c44496573802804849799c8b2f2ec10191016e8
kelar86 <kelar86@mail.ru>
2017-06-11 12:32:44 +0700

    getAllSitesSizeTest

--- webservice/src/test/java/com/gb/statistics/webservice/controller/SiteControllerTest.java
+++ webservice/src/test/java/com/gb/statistics/webservice/controller/SiteControllerTest.java
@@ -1,62 +1,50 @@
 package com.gb.statistics.webservice.controller;
 
 import com.gb.statistics.webservice.AppConfig;
-import com.gb.statistics.webservice.WebAppInitializer;
+import com.gb.statistics.webservice.entity.Site;
+import com.gb.statistics.webservice.repository.MockSiteRepository;
+import com.gb.statistics.webservice.repository.SiteRepository;
 import org.junit.Assert;
-
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
-
-import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.HandlerAdapter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import javax.persistence.PersistenceContext;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
+import java.util.List;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration(classes = {AppConfig.class})
 public class SiteControllerTest {
 
-        //
-        //    @Configuration
-        //    @ComponentScan("com.gb.statistics.webservice.controller")
-        //    @EnableWebMvc
-        //    public class TestAppConfig {
-        //    }
-
-    @PersistenceContext
-    WebAppInitializer webAppInitializer;
     @Autowired
     SiteController siteController;
 
+    @Autowired
+    SiteRepository siteRepository;
 
 
 
     @Test
-    public void getAllSites() throws Exception {
-
+    public void getAllSitesNotNull() throws Exception {
         Assert.assertNotNull(siteController.getAllSites());
-
     }
+
+    @Test
+    public void getAllSitesSizeTest() throws Exception {
+
+        List<Site> allSites = siteRepository.getAll();
+        ResponseEntity<List<Site>> responseEntity = siteController.getAllSites();
+        List<Site> responseBody =  responseEntity.getBody();
+        Assert.assertEquals(allSites.size(), responseBody.size());
+    }
+
+
+
+
 
 }
