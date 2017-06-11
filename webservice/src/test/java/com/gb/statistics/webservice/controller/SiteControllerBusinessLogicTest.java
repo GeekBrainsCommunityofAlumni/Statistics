e6a005501b9f5382617cca28397b46908ac2dc76
kelar86 <kelar86@mail.ru>
2017-06-11 18:05:31 +0700

    getAllSitesTestHttp

--- webservice/src/test/java/com/gb/statistics/webservice/controller/SiteControllerBusinessLogicTest.java
+++ webservice/src/test/java/com/gb/statistics/webservice/controller/SiteControllerBusinessLogicTest.java
@@ -4,21 +4,42 @@ import com.gb.statistics.webservice.AppConfig;
 import com.gb.statistics.webservice.entity.Site;
 import com.gb.statistics.webservice.repository.SiteRepository;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockMultipartHttpServletRequest;
+import org.springframework.stereotype.Controller;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.WebApplicationContext;
 
+import javax.servlet.Servlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration(classes = {AppConfig.class})
-public class SiteControllerTest {
+public class SiteControllerBusinessLogicTest {
+
 
     @Autowired
     SiteController siteController;
@@ -42,8 +63,9 @@ public class SiteControllerTest {
         Assert.assertEquals(allSites.size(), responseBody.size());
     }
 
+    @Test
+    public void getAllSites() throws Exception {
 
-
-
+    }
 
 }
