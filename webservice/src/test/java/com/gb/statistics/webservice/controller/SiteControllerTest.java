5108baa22b4918a3966e71556d63f792a2439552
kelar86 <kelar86@mail.ru>
2017-06-11 12:05:08 +0700

    first test run ok

--- webservice/src/test/java/com/gb/statistics/webservice/controller/SiteControllerTest.java
+++ webservice/src/test/java/com/gb/statistics/webservice/controller/SiteControllerTest.java
@@ -1,7 +1,11 @@
 package com.gb.statistics.webservice.controller;
 
+import com.gb.statistics.webservice.AppConfig;
+import com.gb.statistics.webservice.WebAppInitializer;
 import org.junit.Assert;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,27 +21,41 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.HandlerAdapter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import javax.persistence.PersistenceContext;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(loader = AnnotationConfigContextLoader.class)
-public class SiteControllerTest extends AbstractJUnit4SpringContextTests {
+@WebAppConfiguration
+@ContextConfiguration(classes = {AppConfig.class})
+public class SiteControllerTest {
 
-    @Configuration
-    @ComponentScan("com.gb.statistics.webservice.controller")
-    @EnableWebMvc
-    public class TestAppConfig {
+        //
+        //    @Configuration
+        //    @ComponentScan("com.gb.statistics.webservice.controller")
+        //    @EnableWebMvc
+        //    public class TestAppConfig {
+        //    }
 
-        AnnotationConfigApplicationContext annotationConfigApplicationContext =
-                new AnnotationConfigApplicationContext(SiteController.class);
+    @PersistenceContext
+    WebAppInitializer webAppInitializer;
+    @Autowired
+    SiteController siteController;
 
-    }
+
 
 
     @Test
     public void getAllSites() throws Exception {
+
+        Assert.assertNotNull(siteController.getAllSites());
 
     }
 
