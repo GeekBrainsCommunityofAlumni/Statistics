c3ab653c4f3c3a729d1460e76ea7c78dd391d198
Михаил Агеев macbook <kylesp@rambler.ru>
2017-06-12 21:26:41 +0300

    test

--- src/ios/ios/TableViewController.swift
+++ src/ios/ios/TableViewController.swift
@@ -41,7 +41,7 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        cell.textLabel?.text = "TEST"
+        cell.textLabel?.text = "TEST1"
 
         return cell
     }
