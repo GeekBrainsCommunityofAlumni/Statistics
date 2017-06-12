b52a24583a9826d0d09f0d56dad6b7976009b4c4
kelar86 <kelar86@mail.ru>
2017-06-12 22:27:28 +0700

    setUp() for test

---
+++ webservice/src/test/java/com/gb/statistics/webservice/controller/PersonControllerTest.java
@@ -0,0 +1,37 @@
+package com.gb.statistics.webservice.controller;
+
+import com.gb.statistics.webservice.AppConfig;
+import com.gb.statistics.webservice.entity.Person;
+import com.gb.statistics.webservice.repository.MockPersonRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {AppConfig.class})
+public class PersonControllerTest {
+
+    @Autowired
+    PersonController personController;
+
+    @Before
+    public void setUp(){
+        MockPersonRepository.personsList.add(new Person(1, "Путин"));
+        MockPersonRepository.personsList.add(new Person(2, "Навальный"));
+        MockPersonRepository.personsList.add(new Person(3, "Зюганов"));
+
+    }
+
+    @Test
+    public void getPerson() throws Exception {
+
+    }
+
+}
