9af0e804b080ad39ffd6f5d6e155d0b117693826
gkntnm <gkntnm@gmail.com>
2017-06-12 20:36:18 +0400

    Добавлен метод testDBHelper(). Все тесты DBHelpera пишем сюда.

--- src/crawler/CrawlerTest.java
+++ src/crawler/CrawlerTest.java
@@ -4,10 +4,29 @@ package crawler;
  * Created by Serg on 12.06.2017.
  */
 public class CrawlerTest {
+    private static DBHelper dbHelper;
+
+
+    public static void main(String[] args) {
+        init();
+
+        testDBHelper();
+    }
+
+
+    private static void init() {
+        dbHelper = new DBHelper();
+    }
+
+    private static void testDBHelper() {
+        System.out.println("Подключаемся к MySQL");
+        dbHelper.connectToDB();
+        System.out.println("Вероятно успешно");
 
 
 
-    public static void main(String[] args){
+
 
     }
+
 }
