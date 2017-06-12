fc68922fcb7db3956c389059b3e64cc67fa610ea
uatilman <tefaltron1298juDE>
2017-06-12 23:13:43 +0300

    Все иключения класса IOException, в зависимости от причины исключения выдаются следкющие сообщения:
         1. "Page not found"
         2. "Site not found"
         3. "Internet connection lost"
         4. "Unidentified error"
         5. "robots.txt not found" только для метода downloadRobot
         6. "sitemap not found" только для метода downloadSiteMap
    В классе Downloader добавлена реализация методов downloadRobot(String site) и downloadSiteMap(String sitemapURL)
    
    Добавлен метод isReachable() который проверяет  есть ли подключение к интернету путем запроса сайтов google, yandex, mail

--- src/crawler/Downloader.java
+++ src/crawler/Downloader.java
@@ -3,52 +3,106 @@ package crawler;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.UnknownHostException;
 
-/**
- * Created by Юрий on 06.06.2017.
- * http://javatalks.ru/topics/13271?page=1#62015
- */
 public class Downloader {
-//    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException { //Метод для тестирования класса
+
 //        long t = System.currentTimeMillis();
 //        Downloader downloader = new Downloader();
-//        String url = "https://lenta.ru/";
+//        String url = "http://lenta.ru/robots.txt";
+//        //String url = "https://www.kommersant.ru/robots.txt";
 //        String s = downloader.download(url);
 //        System.out.println(s);
 //        BufferedWriter bw = new BufferedWriter(new FileWriter("1.html"));
 //        bw.write(s);
 //        bw.close();
 //        System.out.println(System.currentTimeMillis() - t);
-//    }
+    }
 
     public String download(String url) throws IOException {
         URL urlAddress = null;
-        StringBuilder builder = new StringBuilder();
+        StringBuilder stringBuilder = new StringBuilder();
 
         try {
             urlAddress = new URL(url);
-        } catch (MalformedURLException e) {
-            throw new IOException(e + "\n " + url + " указан неверно");
+        } catch (MalformedURLException malformedURLException) {
+            throw new IOException("no protocol");
         }
 
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(urlAddress.openConnection().getInputStream()))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlAddress.openConnection().getInputStream()))) {
             int i;
-            while ((i = br.read()) != -1) {
-                builder.append((char) i);
+            while ((i = bufferedReader.read()) != -1) {
+                stringBuilder.append((char) i);
             }
-            br.close();
-        } catch (IOException e) {
-            throw new IOException(e + "\n " + "Ошибка чтения страницы");
+            bufferedReader.close();
+        } catch (FileNotFoundException fileException) {
+            throw new IOException("Page not found");
+        } catch (UnknownHostException hostException) {
+            if (isReachable()) {
+                throw new IOException("Site not found");
+            } else {
+                throw new IOException("Internet connection lost");
+            }
+        } catch (IOException ioException) {
+            throw new IOException("Unidentified error");
         }
-        return builder.toString();
+        return stringBuilder.toString();
     }
 
-    public String downloadRobot(String site) {
+    private static boolean isReachable() {
+        boolean test1;
+        boolean test2;
+        boolean test3;
 
-        return "";
+        try {
+            new BufferedReader(new InputStreamReader(new URL("https://www.google.ru").openConnection().getInputStream()));
+            test1 = true;
+        } catch (IOException e) {
+            test1 = false;
+        } finally {
+            try {
+                new BufferedReader(new InputStreamReader(new URL("https://www.yandex.ru/").openConnection().getInputStream()));
+                test2 = true;
+            } catch (IOException e) {
+                test2 = false;
+            } finally {
+                try {
+                    new BufferedReader(new InputStreamReader(new URL("https://mail.ru/").openConnection().getInputStream()));
+                    test3 = true;
+                } catch (IOException e) {
+                    test3 = false;
+                }
+            }
+        }
+
+        return test1 || test2 || test3;
     }
 
-    public String downloadSiteMap(String sitemapURL) {
-        return "";
+
+    public String downloadRobot(String site) throws IOException {
+        String robotTxt;
+        try {
+            robotTxt = download(site + "/robots.txt");
+        } catch (IOException e) {
+            if (e.getMessage().equals("Page not found")) {
+                throw new IOException("robots.txt not found");
+            } else
+                throw new IOException(e.getMessage());
+        }
+        return robotTxt;
+    }
+
+    public String downloadSiteMap(String sitemapURL) throws IOException {
+        String sitemap;
+        try {
+            sitemap = download(sitemapURL);
+        } catch (IOException e) {
+            if (e.getMessage().equals("Page not found")) {
+                throw new IOException("sitemap not found");
+            } else
+                throw new IOException(e.getMessage());
+        }
+        return sitemap;
     }
 }
