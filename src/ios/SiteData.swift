59a5d6b2c9c776fb825c53f2acf7e88e439ca99f
Михаил Агеев macbook <kylesp@rambler.ru>
2017-06-14 14:34:32 +0300

    Созданы классы для Модели. Написан класс SiteData

---
+++ src/ios/SiteData.swift
@@ -0,0 +1,13 @@
+//
+//  SiteData.swift
+//  
+//
+//  Created by Михаил Агеев macbook on 14.06.17.
+//
+//
+
+import UIKit
+
+class SiteData: - {
+
+}
---
+++ src/ios/ios/SiteDataProvider.swift
@@ -0,0 +1,13 @@
+//
+//  SiteDataProvider.swift
+//  ios
+//
+//  Created by Михаил Агеев macbook on 14.06.17.
+//  Copyright © 2017 GB. All rights reserved.
+//
+
+import UIKit
+
+class SiteDataProvider: NSObject, URLSessionDelegate {
+
+}
