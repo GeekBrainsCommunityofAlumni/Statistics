3f25f4736c22926cf1482eb3aef8ce70c3f095d6
GokenTanmay <gkntnm@gmail.com>
2017-06-15 13:30:31 +0400

    Скрипт загрузки в БД тестовых данных. Можно дописыват и дополнять. В рабочей базе этих данных точно не будет.

---
+++ src/crawler/add_test_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO `statistics`.`persons` (`name`) VALUES ('Иванов Иван Иванович');
+
+INSERT INTO `statistics`.`persons` (`name`) VALUES ('Петров Петр Петрович');
+
+INSERT INTO `statistics`.`persons` (`name`) VALUES ('Сидоров Сидр Сидорович');
