96c112ddb536c82a8c4ec0985ad89b7e28c09358
sd_s <sd_s@icloud.com>
2017-06-15 17:25:40 +0300

    Добавил описание + убрал у общей статистики дату

--- src/ios/ios/DataProvider.swift
+++ src/ios/ios/DataProvider.swift
@@ -9,20 +9,22 @@
 import UIKit
 
 enum DataProviderType {
-    case db, source
+    case db         // кеш
+    case source     //  сеть
 }
 
+// протокол для возврата из источника данных. dataProvider - дает нам возможность в вызывающем методе получить информацию о типе провайдера (кеш/сеть) и т.д.
 protocol DataProviderProtocol {
     func didCompliteRequestOnData(data: [SiteData], date: Date, dataProvider: DataProvider)
-    func didCompliteRequestTotal(data: [SiteData], date: Date, dataProvider: DataProvider)
+    func didCompliteRequestTotal(data: [SiteData], dataProvider: DataProvider)
 }
 
 class DataProvider{
     var type: DataProviderType!
-    let priority: Int = 0
+    //let priority: Int = 0    уже не вижу смысла в этом параметре
     var delegat: DataProviderProtocol!
     
-    func getSumaryData(date: Date){
+    func getSumaryData(){
         
     }
     func getDataOnDate(date: Date){
