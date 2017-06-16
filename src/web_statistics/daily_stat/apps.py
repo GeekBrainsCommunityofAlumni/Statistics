bbc7e1a187855adb0a9545f962730ea3887eaf5c
nassy <stasya.aska@gmail.com>
2017-06-17 02:32:28 +0300

    4 apps: source manager, tag manager, and 2 types stats

---
+++ src/web_statistics/daily_stat/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DailyStatConfig(AppConfig):
+    name = 'daily_stat'
---
+++ src/web_statistics/timerange_stat/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TimerangeStatConfig(AppConfig):
+    name = 'timerange_stat'
