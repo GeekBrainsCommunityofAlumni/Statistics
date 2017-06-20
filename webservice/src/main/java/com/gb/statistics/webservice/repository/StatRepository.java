8362f32b9c1bda6161c6195b6a7f87caaf4c3752
ZeroCool142 <undead142@gmail.com>
2017-06-20 15:37:16 +0300

    Add stat controller

--- webservice/src/main/java/com/gb/statistics/webservice/repository/StatRepository.java
+++ webservice/src/main/java/com/gb/statistics/webservice/repository/StatRepository.java
@@ -5,7 +5,9 @@ import com.gb.statistics.webservice.entity.Person;
 import com.gb.statistics.webservice.entity.Personpagerank;
 import com.gb.statistics.webservice.entity.PersonpagerankPK;
 import com.gb.statistics.webservice.entity.Site;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
 import java.util.List;
@@ -13,6 +15,13 @@ import java.util.List;
 public interface StatRepository extends Repository<Personpagerank, PersonpagerankPK> {
     List<Personpagerank> findByPageSite(Site site);
     List<Personpagerank> findByPageSiteAndPerson(Site site, Person person);
-    List<Personpagerank> findByPageSiteAndPersonAndPageFounddatetime(Site s, Person p, Date d); //AndRankNot Integer 0
+
+    @Query("select p from Personpagerank p " +
+            "where p.page.site = :s and " +
+            "p.person = :pers and " +
+            "date(p.page.founddatetime) = date(:date)")
+    List<Personpagerank> findByPageSiteAndPersonAndPageFounddatetime(@Param("s") Site s,
+                                                                     @Param("pers") Person p,
+                                                                     @Param("date") Date d);
     List<Personpagerank> findByPageSiteAndPersonAndPageFounddatetimeBetween(Site s, Person p, Date d, Date d2);
 }
