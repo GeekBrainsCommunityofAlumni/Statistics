57eec06108763d793037f8ed2bc009d28d3f74b1
nassy <stasya.aska@gmail.com>
2017-06-20 14:12:21 +0300

    added 5 tables to db: Persons, Keywords, Pages, Sites, PersonPageRank

--- src/web_statistics/source_manager/models.py
+++ src/web_statistics/source_manager/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Sites(models.Model):
+    name = models.CharField(max_length=2048, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Pages(models.Model):
+    url = models.CharField(max_length=2048)
+    site_id = models.ForeignKey(Sites)
+    found_date_time = models.DateTimeField()
+    last_scan_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.last_scan_date
--- src/web_statistics/tag_manager/models.py
+++ src/web_statistics/tag_manager/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from source_manager.models import Pages
 
-# Create your models here.
+
+class Persons(models.Model):
+    name = models.CharField(max_length=2048, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Keywords(models.Model):
+    name = models.CharField(verbose_name='key_word', max_length=2048, blank=False)
+    person_id = models.ForeignKey(Persons)
+
+    def __str__(self):
+        return self.name
+
+
+class PersonPageRank(models.Model):
+    person_id = models.ForeignKey(Persons)
+    page_id = models.ForeignKey(Pages)
+    rank = models.IntegerField(blank=True)
+
+    def __str__(self):
+        return self.rank
