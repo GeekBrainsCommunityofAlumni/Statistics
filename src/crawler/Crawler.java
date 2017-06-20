8de8eede7aee3a1239a8ddeb2b9aba2bf0e1e588
GokenTanmay <gkntnm@gmail.com>
2017-06-20 15:45:26 +0400

    Рефактор метода addNewSitesPage() - вынесен в самостоятельный метод.

--- src/crawler/Crawler.java
+++ src/crawler/Crawler.java
@@ -13,21 +13,22 @@ public class Crawler {
     private static ArrayList<Integer> pagesIDForScan;
     private static ArrayList<Integer> personsID;
     private static Downloader downloader;
-    private static ArrayList<String> newSites;
+    //    private static ArrayList<String> newSites;
     private static Parser parser;
     private static DBHelper dbHelper;
 
     public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
         init();
+        addNewSitesToPages();
         crawlsNewSite();
         recrawls();
     }
 
     private static void crawlsNewSite() throws IOException, ParserConfigurationException, SAXException {
-        newSites = dbHelper.getNewSites();
-        if (newSites != null) {
-            addNewSitesToPages(newSites);
-        }
+//        newSites = dbHelper.getNewSites();
+//        if (newSites != null) {
+//            addNewSitesToPages(newSites);
+//        }
         pagesIDForScan = dbHelper.getPagesIDWithoutScanDate(); //Метод возвращает список ID старниц для сканирования. БЕЗ robots.txt и sitemap
         for (int pageID : pagesIDForScan) {
             String pageSource = downloader.download(dbHelper.getUrlPageViaID(pageID));
@@ -52,49 +53,55 @@ public class Crawler {
         personsID = dbHelper.getPersonsID();
     }
 
-    private static void addNewSitesToPages(ArrayList<String> newSites) throws IOException, ParserConfigurationException, SAXException { // Решить что делать с пробрасываемыми исключениями. throws IOException, ParserConfigurationException, SAXException
-        for (String site : newSites) {
-            String robotTxt = null;
-            ArrayList<String> urlPages = new ArrayList<>();
+    private static void addNewSitesToPages() throws IOException, ParserConfigurationException, SAXException { // Решить что делать с пробрасываемыми исключениями. throws IOException, ParserConfigurationException, SAXException
+        ArrayList<String> newSites = dbHelper.getNewSites();
+        System.out.println("Новых сайтов в базе " + newSites.size());
+        if (newSites.size()>0) {
 
-            try {
-                robotTxt = downloader.downloadRobot(site);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            String sitemapURL = null;
+            for (String site : newSites) {
+                String robotTxt = null;
+                ArrayList<String> urlPages = new ArrayList<>();
 
-            if (robotTxt != null) {
-                sitemapURL = parser.parseRobotTxt(robotTxt);
-            }
+                try {
+                    robotTxt = downloader.downloadRobot(site);
+                    System.out.println("Robots.txt содержит:");
+                    System.out.println(robotTxt);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String sitemapURL = null;
 
-            if (sitemapURL != null) {
-                String sitemap = null;
-                sitemap = downloader.downloadSiteMap(sitemapURL);
-                if (isItSitemapIndex(sitemap)) {
-                    ArrayList<String> sitemapsURL = parser.parseSiteMapIndex(sitemap);
-                    for (String sitemapUrl : sitemapsURL) {
-                        sitemap = downloader.downloadSiteMap(sitemapUrl);
-                        System.gc();
-                        ArrayList<String> urlPagesSiteMapIndex = parser.parseSiteMap(sitemap);
-                        long mem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-                        for (String url : urlPagesSiteMapIndex) {
-                            urlPages.add(url);
+                if (robotTxt != null) {
+                    sitemapURL = parser.parseRobotTxt(robotTxt);
+                    System.out.println("Найден sitemap.xml : " + sitemapURL);
+                }
+
+                if (sitemapURL != null) {
+                    String sitemap = null;
+                    sitemap = downloader.downloadSiteMap(sitemapURL);
+                    if (isItSitemapIndex(sitemap)) {
+                        ArrayList<String> sitemapsURL = parser.parseSiteMapIndex(sitemap);
+                        for (String sitemapUrl : sitemapsURL) {
+                            sitemap = downloader.downloadSiteMap(sitemapUrl);
+                            System.gc();
+                            ArrayList<String> urlPagesSiteMapIndex = parser.parseSiteMap(sitemap);
+                            long mem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+                            for (String url : urlPagesSiteMapIndex) {
+                                urlPages.add(url);
+                            }
+                            dbHelper.addPagesToSite(urlPages, site);
+                            urlPages.clear();
                         }
-                        dbHelper.addPagesToSite(urlPages, site);
-                        urlPages.clear();
                     }
-                }
 
-                if (isItSitemap(sitemap)) {
-                    urlPages = parser.parseSiteMap(sitemap);
-                    dbHelper.addPagesToSite(urlPages, site);
+                    if (isItSitemap(sitemap)) {
+                        urlPages = parser.parseSiteMap(sitemap);
+                        dbHelper.addPagesToSite(urlPages, site);
+                    }
+                } else {
+                    //TODO Сайтмэп не найден - здесь будет функция построения карты сайта без сайтмэпа
                 }
-            } else {
-                //TODO Сайтмэп не найден - здесь будет функция построения карты сайта без сайтмэпа
             }
-
-
         }
     }
 
