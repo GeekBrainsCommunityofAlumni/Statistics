1d9d0f0a85b80946f14bd79ec83321979d68302d
Cappoocha <katerinacappuccino@gmail.com>
2017-06-22 01:13:27 +0400

    Создан класс, хранящий названия методов REST API

---
+++ src/main/java/desktop_ui/Module/Entity/RestApiMethod.java
@@ -0,0 +1,15 @@
+package desktop_ui.Module.Entity;
+/**
+ * Created by Cappoocha on 22.06.2017.
+ */
+
+/**
+ * Класс, хранящий названия методов REST API
+ */
+public class RestApiMethod
+{
+    public static final String SITES_PATH = "/site";
+    public static final String SITE_ID_PATH = "/site/{id}";
+    public static final String PERSONS_PATH = "/person";
+    public static final String PERSON_ID_PATH = "/person/{id}";
+}
