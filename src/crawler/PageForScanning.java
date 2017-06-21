e12fa12470f1e5885adc2825477c82e511938a82
GokenTanmay <gkntnm@gmail.com>
2017-06-21 15:30:52 +0400

    Класс для работы класса CrawlerTask, содержит ID и URL страницы.

--- src/crawler/PageForScanning.java
+++ src/crawler/PageForScanning.java
@@ -4,4 +4,14 @@ package crawler;
  * Created by Serg on 21.06.2017.
  */
 public class PageForScanning {
+    private int pageID;
+    private String url;
+
+    public PageForScanning(int pageID, String url){
+        this.pageID = pageID;
+        this.url = url;
+    }
+
+    public int getPageID(){return pageID;}
+    public String getUrl(){return url;}
 }
