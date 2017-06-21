8ada23be3cb2bfa526ed9a6ccb413d00147ab4d2
GokenTanmay <gkntnm@gmail.com>
2017-06-21 15:27:32 +0400

    Класс содержащий в себе задачу для потока краулера. Поток краулела запрашивает данную задачу из потокобезопасного репозитория.

--- src/crawler/CrawlerTask.java
+++ src/crawler/CrawlerTask.java
@@ -1,7 +1,34 @@
 package crawler;
 
+import java.util.ArrayList;
+
 /**
  * Created by Serg on 21.06.2017.
  */
 public class CrawlerTask {
+    private ArrayList<Person> personList = new ArrayList<>();
+    private PageForScanning pageForScanning;
+
+
+    public void addPerson(Person person) {
+        personList.add(person);
+    }
+
+    public void addPageForScanning(PageForScanning pageForScanning){
+        this.pageForScanning = pageForScanning;
+    }
+
+    public ArrayList<Person> getPersonList() {
+        return personList;
+    }
+
+    public PageForScanning getPage() {
+        return pageForScanning;
+    }
+
+    public void addPersonList(ArrayList<Person> personArrayList) {
+        if(personArrayList != null){
+            this.personList.addAll(personArrayList);
+        }
+    }
 }
