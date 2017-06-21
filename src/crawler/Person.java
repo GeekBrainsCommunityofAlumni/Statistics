88aaea5eddc48b71e60eb476d692e9ddbbbf50c0
GokenTanmay <gkntnm@gmail.com>
2017-06-21 15:35:20 +0400

    Класс используемый в CrawlerTask. Содержит PersonID и список ключевых слов для данной персоны.

--- src/crawler/Person.java
+++ src/crawler/Person.java
@@ -1,7 +1,21 @@
 package crawler;
 
+import java.util.ArrayList;
+
 /**
  * Created by Serg on 21.06.2017.
  */
 public class Person {
+
+    private int ID;
+    private ArrayList<String> keywordsList = new ArrayList<>();
+
+    public Person(int personID, ArrayList<String> personKeywords) {
+        this.ID = personID;
+        this.keywordsList = personKeywords;
+    }
+
+    public int getID(){return this.ID;}
+
+    public ArrayList<String> getKeywordsList(){return keywordsList;}
 }
