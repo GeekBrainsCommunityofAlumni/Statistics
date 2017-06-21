fb500fd7bbeda16f13c9b258ee881653d8c6de2c
Михаил Агеев macbook <kylesp@rambler.ru>
2017-06-21 22:03:25 +0300

    Реализован полностью контролеер выбора данных для ежедневной статистики

--- src/ios/ios/TableViewControllerDayStatDV.swift
+++ src/ios/ios/TableViewControllerDayStatDV.swift
@@ -9,15 +9,15 @@
 import UIKit
 
 class TableViewControllerDayStatDV: UITableViewController {
+    
+    var choosenSite: String = "Default Name"
+    var choosenDate1: Date = Date()
+    var choosenDate2: Date = Date()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+       print("\(self.choosenDate1) \(self.choosenDate2) \(self.choosenSite)")
     }
 
     override func didReceiveMemoryWarning() {
--- src/ios/ios/ViewControllerDayStat.swift
+++ src/ios/ios/ViewControllerDayStat.swift
@@ -8,28 +8,98 @@
 
 import UIKit
 
-class ViewControllerDayStat: UIViewController {
+class ViewControllerDayStat: UIViewController, DataManagerProtocol, UIPickerViewDelegate, UIPickerViewDataSource  {
 
+    @IBOutlet weak var pickerSite: UIPickerView!
+    @IBOutlet weak var pickerDate1: UIDatePicker!
+    @IBOutlet weak var pickerDate2: UIDatePicker!
+    
+    var dm = DataManager.initWithFakeManager()
+    
+    
+    var choosenSite: String = ""
+    var choosenDate1: Date = Date()
+    var choosenDate2: Date = Date()
+    
+    
+    
+    var data: [SiteData] = []{
+        didSet{
+            self.sites = self.uniqueSite(data: data)
+        }
+    }
+    
+    var sites: [String] = []{
+        didSet{
+            pickerSite.reloadInputViews()
+        }
+    }
+    
+    func uniqueSite(data: [SiteData]) -> [String]{
+        var arrayOfSiteName: [String] = []
+        for item in data {
+            arrayOfSiteName.append(item.site)
+        }
+        arrayOfSiteName = Array(Set(arrayOfSiteName))
+        return arrayOfSiteName
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        dm.delegat = self
+        dm.getSumaryData()
+        pickerSite.delegate = self
+        pickerSite.dataSource = self
+        self.choosenSite = self.sites[0]
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // ВЫБОР САЙТА
+    //количество секций
+    func numberOfComponents(in pickerView: UIPickerView) -> Int{
+        return 1
+    }
+    //количество полей в крутилке
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        //количество сайтов
+        return sites.count
+        //return arraySite.count
+    }
+    
+    
+    //метод который реализует взаимодействие при выборе
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
+        // по идеи должны сохранять выбор, чтобы в последствии его передать
+        choosenSite = sites[row]
+        
+    }
+    //название полей крутилки :)
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?{
+        // вывод названия сайтов
+        return sites[row]
+    }
+    // ВЫБОР САЙТА ЗАВЕРШЕН
+    
+    func didCompliteRequestOnData(data: [SiteData], date1: Date, date2: Date){
+        //self.data = data
+    }
+    
+    func didCompliteRequestTotal(data: [SiteData]){
+        self.data = data
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ShowStat"{
+            let destinatinVC: TableViewControllerDayStatDV = segue.destination as! TableViewControllerDayStatDV
+            destinatinVC.choosenSite = self.choosenSite
+            destinatinVC.choosenDate1 = self.pickerDate1.date
+            destinatinVC.choosenDate2 = self.pickerDate2.date
+        }
+        else{
+            
+        }
     }
-    */
+
 
 }
