1acfc3c8e1ddf56b6db0cc1f6df6892cd0537c9c
nassy <stasya.aska@gmail.com>
2017-06-22 12:35:34 +0300

    commit repeated, cos smth goes wrong

--- src/web_statistics/user_management/views.py
+++ src/web_statistics/user_management/views.py
@@ -2,6 +2,9 @@ from django.contrib import auth
 from django.shortcuts import render, HttpResponseRedirect
 from django.http import Http404
 from django.contrib.auth.models import User
+from tag_manager.models import Persons, Keywords
+from source_manager.models import Sites, Pages
+from django.contrib.auth.decorators import login_required
 # from user_management.models import Person
 from django.core.exceptions import ValidationError
 # from user_management.forms import MyRegistrationForm
@@ -75,3 +78,10 @@ def registration(request):
         user.save()
         return HttpResponseRedirect("/privateroom/")
     return render(request, "registration.html")
+
+@login_required(login_url='/privateroom/')
+def common_statistics(request):
+    sites = Sites.objects.order_by('name')
+    persons = Persons.objects.order_by('name')
+    keywords = Keywords.objects.order_by('name')
+    return render(request, "incl_daily_st.html", {'persons': persons, 'keywords': keywords, 'sites': sites})
