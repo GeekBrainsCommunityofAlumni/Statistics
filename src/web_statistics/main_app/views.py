745ccce603148f8a1f45b55641491be797c146d7
nassy <stasya.aska@gmail.com>
2017-06-24 19:10:21 +0300

    fixed common statistics

--- src/web_statistics/main_app/views.py
+++ src/web_statistics/main_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from tag_manager.models import Persons, Keywords, PersonPageRank
 from source_manager.models import Sites, Pages
 from django.contrib.auth.decorators import login_required
+from django.db.models import Sum
+from pprint import pprint
 
 
 def main(request):
@@ -17,7 +19,9 @@ def daily_statistics(request):
     sites = Sites.objects.order_by('name')
     persons = Persons.objects.order_by('name')
     keywords = Keywords.objects.order_by('name')
-    return render(request, 'daily_statistics.html', {'persons': persons, 'keywords': keywords, 'sites': sites})
+    person_ranks = PersonPageRank.objects.values('person_id_id', 'page_id_id').annotate(rank=Sum('rank'))
+    return render(request, 'daily_statistics.html', {'persons': persons, 'keywords': keywords,
+                                                     'sites': sites, 'person_ranks': person_ranks})
 
 
 @login_required(login_url='/privateroom/')
@@ -25,9 +29,9 @@ def periodic_statistics(request):
     sites = Sites.objects.order_by('name')
     persons = Persons.objects.order_by('name')
     keywords = Keywords.objects.order_by('name')
-    ranks = PersonPageRank.objects.all()
+    person_ranks = PersonPageRank.objects.values('person_id_id', 'page_id_id').annotate(rank=Sum('rank'))
     return render(request, 'periodic_statistics.html', {'persons': persons, 'keywords': keywords,
-                                                        'sites': sites, 'ranks': ranks})
+                                                        'sites': sites, 'person_ranks': person_ranks})
 
 
 def registration(request):
