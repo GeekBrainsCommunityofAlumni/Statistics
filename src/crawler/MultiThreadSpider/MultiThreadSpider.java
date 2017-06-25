c1a28da7320da871af6308692b4f6c9be2e9e6cc
gkntnm <gkntnm@gmail.com>
2017-06-25 20:45:31 +0400

    Рефактор. Надо менять архетектуру паука.

--- src/crawler/MultiThreadSpider/MultiThreadSpider.java
+++ src/crawler/MultiThreadSpider/MultiThreadSpider.java
@@ -1,10 +1,13 @@
 package crawler.MultiThreadSpider;
 
+import crawler.DBHelper;
+import crawler.Downloader;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
 
 /**
@@ -12,41 +15,57 @@ import java.util.HashSet;
  */
 public class MultiThreadSpider {
     static String scanSite;
+    static int linkLimit;
     static HashSet<String> notScanedURLs = new HashSet<>();
     static HashSet<String> tmpScanedURLs = new HashSet<>();
     static HashSet<String> scanedURLs = new HashSet<>();
+    static Downloader downloader = new Downloader();
 
-    public static void main(String[] args){
-        scanSite = "http://lenta.ru";
-        run();
-        print();
-    }
 
-    public void MultiThreadSpider(String scanSite) {
+//    public static void main(String[] args){
+//        scanSite = "http://lenta.ru";
+//        linkLimit = 100;
+//        run();
+//        print();
+//    }
+
+    public MultiThreadSpider(String scanSite, int linkLimit) {
         this.scanSite = scanSite;
+        this.linkLimit = linkLimit;
     }
 
     public static void run() {
         notScanedURLs.add(scanSite);
-        while (notScanedURLs.size() > 0) {
+        while ((notScanedURLs.size() > 0) && (scanedURLs.size() <= linkLimit)) {
             Document doc = null;
             String url = notScanedURLs.iterator().next();
 
             try {
-                doc = Jsoup.connect(url).get();
+                //System.out.println("URL: " + url);
+                String html = downloader.download(url);
+                //System.out.println("HTML length: " + html.length());
+                doc = Jsoup.parse(html);
+                doc.absUrl(url);
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
             for (Element element : doc.select("a")) {
-                if (element.attr("href").startsWith("/")) {
-                    tmpScanedURLs.add(element.attr("abs:href"));
+
+
+                if (element.attr("href") != "") {
+                    //System.out.println(element.attr("href").startsWith("/"));
+                    if (element.attr("href").startsWith("/")) {
+                        //System.out.println(element);
+                        tmpScanedURLs.add(url + element.attr("href"));
+                    }
                 }
             }
             notScanedURLs.remove(url);
-            System.out.println(url);
-            System.out.println(scanedURLs.size());
+            //System.out.println(url);
+            //System.out.println(scanedURLs.size());
             scanedURLs.add(url);
+
 
             for (String tmpURL : tmpScanedURLs) {
                 if (!scanedURLs.contains(tmpURL)) {
@@ -56,7 +75,11 @@ public class MultiThreadSpider {
         }
     }
 
-    public static void print(){
+    public static void print() {
         System.out.println(scanedURLs);
+    }
+
+    public static ArrayList<String> getScanedURLs() {
+        return new ArrayList<>(scanedURLs);
     }
 }
