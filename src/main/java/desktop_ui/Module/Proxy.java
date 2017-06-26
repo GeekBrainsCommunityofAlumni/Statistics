879dfa795d888a8d54efe1607f7929ea4c88201c
Cappoocha <katerinacappuccino@gmail.com>
2017-06-26 16:56:19 +0400

    Удалены ненужные классы и методы

--- src/main/java/desktop_ui/Module/Proxy.java
+++ src/main/java/desktop_ui/Module/Proxy.java
@@ -1,54 +1,16 @@
 package desktop_ui.Module;
-import desktop_ui.Module.DTO.CommonStatisticResultItem;
+
 import desktop_ui.Module.DTO.DailyStatisticResultItem;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 import java.time.LocalDate;
-import java.time.Period;
-import java.util.Date;
 
 /**
  * Created by Cappoocha on 20.06.2017.
  */
 public class Proxy
 {
-    /**
-     * Возвращает список с названиями доступных сайтов
-     *
-     * @return ObservableList
-     */
-    public static ObservableList<String> getAvailableSiteList()
-    {
-        return FXCollections.observableArrayList("Lenta.ru", "Vk.com");
-    }
-
-    /**
-     * Вовзращает список с именами доступных личностей
-     *
-     * @return ObservableList
-     */
-    public static ObservableList<String> getAvailablePersonNameList()
-    {
-        return FXCollections.observableArrayList("Путин", "Медведев", "Навальный");
-    }
-
-    /**
-     * Возвращает общую статистику по сайту
-     *
-     * @param siteId
-     *
-     * @return ObservableList
-     */
-    public static ObservableList<CommonStatisticResultItem> getCommonStatistic(Integer siteId)
-    {
-        return FXCollections.observableArrayList(
-                new CommonStatisticResultItem(1, "Путин"),
-                new CommonStatisticResultItem(10, "Медведев"),
-                new CommonStatisticResultItem(8, "Навальный")
-        );
-    }
-
     /**
      * Возвращает ежедневную статистику по сайту
      *
