e59be506f315ccc7cc7147cf6fa2989fa519d73c
nassy <stasya.aska@gmail.com>
2017-06-27 14:53:53 +0300

    fixed merge problems

--- src/web_statistics/source_manager/models.py
+++ src/web_statistics/source_manager/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+
+class Sites(models.Model):
+    name = models.CharField(max_length=2048, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Pages(models.Model):
+    url = models.CharField(max_length=2048)
+    site_id = models.ForeignKey(Sites)
+    found_date_time = models.DateTimeField()
+    last_scan_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.url
+
+
+admin.site.register(Sites)
